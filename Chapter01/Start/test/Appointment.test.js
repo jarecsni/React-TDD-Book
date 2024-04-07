8b4f4355a2268615024ebdd6fd6eb02198b22365
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-04-07 16:44:43 +0100

    :recycle: Refactor render function in Appointment.test.js

--- Chapter01/Start/test/Appointment.test.js
+++ Chapter01/Start/test/Appointment.test.js
@@ -3,26 +3,27 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { act } from '@testing-library/react';
 
-const render = (component, container) => {  
-    act(() => {
-        createRoot(container).render(component);
-    });
-}
+
 
 describe('Appointment', () => {
     let container;
     let customer
+    const render = (component) => {  
+        act(() => {
+            createRoot(container).render(component);
+        });
+    }
     beforeEach(() => {
         container = document.createElement('div');
     });
     it('renders the customer first name', () => {
         customer = { firstName: 'Ashley' };
-        render(<Appointment customer={customer} />, container);
+        render(<Appointment customer={customer} />);
         expect(container.textContent).toContain('Ashley');
     });
     it('renders another customer first name', () => {
         customer = { firstName: 'Jordan' };
-        render(<Appointment customer={customer} />, container);
+        render(<Appointment customer={customer} />);
         expect(container.textContent).toContain('Jordan');
     });
 });
