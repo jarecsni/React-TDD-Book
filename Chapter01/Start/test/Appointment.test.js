d2922da557a105d0e5a39bc4211532bde32706ac
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-04-07 16:33:25 +0100

    :recycle: Drying up test code

--- Chapter01/Start/test/Appointment.test.js
+++ Chapter01/Start/test/Appointment.test.js
@@ -3,23 +3,26 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { act } from '@testing-library/react';
 
+const render = (component, container) => {  
+    act(() => {
+        createRoot(container).render(component);
+    });
+}
+
 describe('Appointment', () => {
+    let container;
+    let customer
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
     it('renders the customer first name', () => {
-        const container = document.createElement('div');
-        const root = createRoot(container);
-        const customer = { firstName: 'Ashley' };
-        act(() => {
-            root.render(<Appointment customer={customer} />);
-        });
+        customer = { firstName: 'Ashley' };
+        render(<Appointment customer={customer} />, container);
         expect(container.textContent).toContain('Ashley');
     });
     it('renders another customer first name', () => {
-        const container = document.createElement('div');
-        const root = createRoot(container);
-        const customer = { firstName: 'Jordan' };
-        act(() => {
-            root.render(<Appointment customer={customer} />);
-        });
+        customer = { firstName: 'Jordan' };
+        render(<Appointment customer={customer} />, container);
         expect(container.textContent).toContain('Jordan');
     });
 });
