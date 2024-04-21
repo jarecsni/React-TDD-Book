e83b0f5e622fea32208f99e09edf516d30a64794
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-04-21 16:11:28 +0100

    ✅ Render each appt in a li

--- Chapter01/Start/test/Appointment.test.js
+++ Chapter01/Start/test/Appointment.test.js
@@ -50,6 +50,17 @@ describe('AppointmentsDayView', () => {
         expect(container.querySelector('ol')).not.toBeNull();
         expect(container.querySelector('ol').children).toHaveLength(2);
     });
+    it('renders each appointment in an li', () => {
+        const today = new Date();
+        const appointments = [
+            { startsAt: today.setHours(12, 0), customer: { firstName: 'Ashley' } },
+            { startsAt: today.setHours(13, 0), customer: { firstName: 'Jordan' } }
+        ];
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelectorAll('li')).toHaveLength(2);
+        expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+        expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+    });
 });
 
 
